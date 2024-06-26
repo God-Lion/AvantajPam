f6f09c3cf61b90b854a4db1e6fb5eb79d9c9923b
God-Lion <borneluszico@gmail.com>
2024-06-26 07:38:42 -0400

    setup  interface to manifest

--- app/src/main/java/ht/AvantajPam/app/MainActivity.java
+++ app/src/main/java/ht/AvantajPam/app/MainActivity.java
@@ -1,12 +1,19 @@
 package ht.AvantajPam.app;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner countryCodeSpinner = findViewById(R.id.country_code_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.country_codes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countryCodeSpinner.setAdapter(adapter);
     }
 }
