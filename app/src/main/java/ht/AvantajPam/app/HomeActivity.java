1907cb7b22a8d9d782fb43ab67291cfc86d61bf6
God-Lion <borneluszico@gmail.com>
2024-06-26 15:28:53 -0400

    setup connection to first  interface

--- app/src/main/java/ht/AvantajPam/app/HomeActivity.java
+++ app/src/main/java/ht/AvantajPam/app/HomeActivity.java
@@ -1,15 +1,56 @@
 package ht.AvantajPam.app;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ht.AvantajPam.app.Adapter.SlideAdapter;
 
 public class HomeActivity extends AppCompatActivity {
+    private ViewPager2 viewPager;
+    private Handler handler = new Handler();
+    private Runnable runnable;
+    private int currentPage = 0;
 
     @Override
     protected void onCreate ( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        viewPager = findViewById(R.id.viewPager);
 
+        List<Integer> imageList = new ArrayList<>();
+        imageList.add(R.drawable.avantajpam);
+        imageList.add(R.drawable.backheader);
+//        imageList.add(R.drawable.image1);
+//        imageList.add(R.drawable.image2);
+//        imageList.add(R.drawable.image3);
+        // Add more images as needed
+
+        SlideAdapter adapter = new SlideAdapter(imageList);
+        viewPager.setAdapter(adapter);
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (currentPage == adapter.getItemCount()) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+                handler.postDelayed(this, 3000); // Slide duration of 3 seconds
+            }
+        };
+        handler.postDelayed(runnable, 3000);
+
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
     }
 }
--- app/src/main/java/ht/AvantajPam/app/SMSVerificationActivity.java
+++ app/src/main/java/ht/AvantajPam/app/SMSVerificationActivity.java
@@ -2,9 +2,12 @@ package ht.AvantajPam.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import ht.AvantajPam.app.fragment.ButtonNumberPad;
 
 public class SMSVerificationActivity extends AppCompatActivity {
 
@@ -12,10 +15,17 @@ public class SMSVerificationActivity extends AppCompatActivity {
     protected void onCreate ( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms_verification);
-        Button btnOk = findViewById(R.id.btnOk);
-        btnOk.setOnClickListener(view -> onButtonNextAction());
+        Log.d("DEBUG", getResources().getConfiguration().orientation + "");
+        if (savedInstanceState == null) {
+            ButtonNumberPad firstFragment = new ButtonNumberPad();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.fragment_container_view, firstFragment);
+            ft.commit();
+        }
     }
+
     private void onButtonNextAction () {
         startActivity(new Intent(this, CreateSecretCodeActivity.class));
     }
+
 }
