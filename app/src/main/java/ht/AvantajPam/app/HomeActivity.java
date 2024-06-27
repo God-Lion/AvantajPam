0c0b413ce1790459121f61e3ec2d2d3efc80fcc1
God-Lion <borneluszico@gmail.com>
2024-06-26 21:16:27 -0400

    not complete

--- app/src/main/java/ht/AvantajPam/app/HomeActivity.java
+++ app/src/main/java/ht/AvantajPam/app/HomeActivity.java
@@ -18,30 +18,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
-    private ViewPager2 viewPager;
-    private Handler handler = new Handler();
-    private Runnable runnable;
-    private int currentPage = 0;
-
     @Override
     protected void onCreate ( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        ImageSlider imageSlider = findViewById(R.id.slider);
-        ScaleTypes scaleType = ScaleTypes.FIT;
-
-        List<SlideModel> slideModels = new ArrayList<>();
-        slideModels.add(new SlideModel("https://p.ecopetit.cat/wpic/lpic/26-263518_tumblr-photography-wallpaper-rocks-on-earth-background.jpg", ScaleTypes.valueOf("1 Image")));
-        slideModels.add(new SlideModel("https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg", ScaleTypes.valueOf("2 Image")));
-        slideModels.add(new SlideModel("https://live.staticflickr.com/7006/6621416427_8504865e6a_z.jpg", ScaleTypes.valueOf("3 Image")));
-        slideModels.add(new SlideModel("https://c4.wallpaperflare.com/wallpaper/662/618/496/natur-2560x1600-sceneries-wallpaper-preview.jpg", ScaleTypes.valueOf("4 Image")));
-        imageSlider.setImageList(slideModels,scaleType);
-
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setBackgroundColor(Color.TRANSPARENT);
-        Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(4);
-        menuItem.setEnabled(false);
+//        ImageSlider imageSlider = findViewById(R.id.slider);
+//        ScaleTypes scaleType = ScaleTypes.FIT;
+//
+//        List<SlideModel> slideModels = new ArrayList<>();
+//        slideModels.add(new SlideModel("https://p.ecopetit.cat/wpic/lpic/26-263518_tumblr-photography-wallpaper-rocks-on-earth-background.jpg", ScaleTypes.valueOf("1 Image")));
+//        slideModels.add(new SlideModel("https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg", ScaleTypes.valueOf("2 Image")));
+//        slideModels.add(new SlideModel("https://live.staticflickr.com/7006/6621416427_8504865e6a_z.jpg", ScaleTypes.valueOf("3 Image")));
+//        slideModels.add(new SlideModel("https://c4.wallpaperflare.com/wallpaper/662/618/496/natur-2560x1600-sceneries-wallpaper-preview.jpg", ScaleTypes.valueOf("4 Image")));
+//        imageSlider.setImageList(slideModels,scaleType);
+//
+//        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+//        bottomNavigationView.setBackgroundColor(Color.TRANSPARENT);
+//        Menu menu = bottomNavigationView.getMenu();
+//        MenuItem menuItem = menu.getItem(4);
+//        menuItem.setEnabled(false);
 
     }
 }
